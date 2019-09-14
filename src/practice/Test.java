2f072cf5ce7d77b058e68f0d80f97a16c9b6c470
santhoshma <santhoshmaidam23@gmail.com>
2019-09-14 22:50:42 +0530

    first commit practice

---
+++ src/practice/Test.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Test {
+public static void main(String[] args) {
+	int a=10,b=15;
+	int res=a+b;
+	System.out.println(res);
+	System.out.println(" Git Hub Trail Test :");
+}
+}
---
+++ src/practice/Test1.java
@@ -0,0 +1,7 @@
+package practice;
+public class Test1 {
+public static void main(String[] args) {
+	System.out.println(" Test1 : Git Hub - Operations :");
+	System.out.println(" Test1 : Git Hub_1 ");
+}
+}
