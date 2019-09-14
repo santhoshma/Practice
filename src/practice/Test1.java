a6b61ddde068ea85a7409c7bae2613e7bbd77bab
santhoshma <santhoshmaidam23@gmail.com>
2019-09-14 23:17:30 +0530

    second commit in git

--- src/practice/Test1.java
+++ src/practice/Test1.java
@@ -2,6 +2,6 @@ package practice;
 public class Test1 {
 public static void main(String[] args) {
 	System.out.println(" Test1 : Git Hub - Operations :");
-	System.out.println(" Test1 : Git Hub_1 ");
+	//System.out.println(" Test1 : Git Hub_1 ");
 }
 }
